17303c1bdd71006c29f7f3a3759865f18ddd5cb5
cwestbrook <calebwestbrook01@gmail.com>
2026-04-14 09:25:26 -0400

    0.05: Added LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,6 +9,8 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
+        if self.props is None:
+            return ""
         string = ""
         for i in self.props:
             string += f' {i}="{self.props[i]}"'
@@ -16,3 +18,20 @@ class HTMLNode:
 
     def __repr__(self):
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value 
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        
+    def __repr__(self):
+        return f"{self.tag}, {self.value}, {self.props}"
+    
