0c553d66f1d933d2d273439a04e9b446c81257bd
cwestbrook <calebwestbrook01@gmail.com>
2026-04-14 10:17:29 -0400

    0.07: Added TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,14 +1,20 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("LEARN TO CODE", TextType.BOLD)
         node2 = TextNode("Death by numbers", TextType.ITALIC)
-        node3 = TextNode("Dial Up", TextType.PLAIN, "spotify.com")
+        node3 = TextNode("Dial Up", TextType.TEXT, "spotify.com")
         self.assertNotEqual(node, node2, node3)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
