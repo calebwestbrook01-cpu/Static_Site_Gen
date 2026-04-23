ab1a3e879b9b895a26e7b5a4a046f45ae31da60f
cwestbrook <calebwestbrook01@gmail.com>
2026-04-23 11:40:12 -0400

    1.0: Ready for Deployment

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return final
     raise Exception("No header")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         from_file = f.read()
@@ -20,17 +20,18 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_file)
     titled_html = template_file.replace("{{ Title }}", title)
     final_html = titled_html.replace("{{ Content }}", html_string)
+    final_html = final_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as w:
         w.write(final_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     files = os.listdir(dir_path_content)
     for f in files:
         source = os.path.join(dir_path_content, f)
         dest = os.path.join(dest_dir_path, f)
         if os.path.isfile(source):
             final_dest = Path(dest).with_suffix(".html")
-            generate_page(source, template_path, final_dest)
+            generate_page(source, template_path, final_dest, basepath)
         else:
-            generate_pages_recursive(source, template_path, dest)
+            generate_pages_recursive(source, template_path, dest, basepath)
